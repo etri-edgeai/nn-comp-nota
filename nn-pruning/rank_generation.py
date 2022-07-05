7e3da68b78a74b1ba3436dfa7bd7ada8d7128bf8
thkim <thkim@nota.ai>
2022-07-05 18:49:05 +0900

    nothing special

--- nn-pruning/rank_generation.py
+++ nn-pruning/rank_generation.py
@@ -33,7 +33,7 @@ parser.add_argument(
 parser.add_argument(
     '--arch',
     type=str,
-    default='resnet_56',
+    default='resnet_110',
     choices=('resnet_50','vgg_16_bn','resnet_56','resnet_110','densenet_40','googlenet'),
     help='The architecture to prune')
 parser.add_argument(

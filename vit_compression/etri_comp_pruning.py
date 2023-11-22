7f0d79d59fd0dfff40f7b81457f36d422b4f2360
root <jinhyeok.park@nota.ai>
2023-11-22 11:40:19 +0000

    Feature/l2norm

--- vit_compression/etri_comp_pruning.py
+++ vit_compression/etri_comp_pruning.py
@@ -1,7 +1,7 @@
-from netspresso.compressor import ModelCompressor, Task, Framework
+from netspresso.compressor import ModelCompressor, Task, Framework, CompressionMethod, RecommendationMethod, Options
 
 
-def model_compression(args):
+def model_compression(num, compression_type="l2norm"):
     compressor = ModelCompressor(email=args.email, password=args.password)
 
     # Upload Model
@@ -18,6 +18,24 @@ def model_compression(args):
         file_path=UPLOAD_MODEL_PATH,
         input_shapes=INPUT_SHAPES,
     )
+
+    if compression_type == "l2norm":
+        COMPRESSED_MODEL_NAME = "l2norm_vit"
+        COMPRESSION_METHOD = CompressionMethod.PR_L2
+        RECOMMENDATION_METHOD = RecommendationMethod.SLAMP
+        RECOMMENDATION_RATIO = 0.2
+
+        OUTPUT_PATH = f"compressed_vit_l2norm_{num}.pt"
+        opt=Options(group_policy='none')
+        compressed_model = compressor.recommendation_compression(
+            model_id=model.model_id,
+            model_name=COMPRESSED_MODEL_NAME,
+            compression_method=COMPRESSION_METHOD,
+            recommendation_method=RECOMMENDATION_METHOD,
+            recommendation_ratio=RECOMMENDATION_RATIO,
+            output_path=OUTPUT_PATH,
+            options=opt,
+        )
 
 def main(args):
     model_compression = "not implemented yet"
